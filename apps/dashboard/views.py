b64925eacaf928ac69130ed71f62435d55965b13
Kendall Cole <kendall@veenome.com>
2015-03-09 21:24:09 +0000

    Fix message issues

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -11,7 +11,8 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.conf import settings
 
-from apps.dashboard.utils import connect_db, search_es, send_mail
+from apps.dashboard.utils import search_es, send_mail
+from apps.profile.utils import connect_mongodb
 from apps.dashboard.forms import IndustryForm, MessageForm
 from apps.profile.models import Authorized, Profile
 
@@ -28,7 +29,7 @@ def dashboard(request, client_code):
 	if request.POST:
 		form = IndustryForm(request.POST)
 		if form.is_valid():
-			db = connect_db('MONGOLAB_URI', 'APP_NAME')
+			db = connect_mongodb()
 			client_short_name = db.clients.find_one({'client_code':client_code})['short_name']
 			query = form.cleaned_data['industry']
 			mentors = search_es(query, client_short_name)
@@ -36,49 +37,62 @@ def dashboard(request, client_code):
 			print form.errors
 	else:
 		form = IndustryForm()
-	return render_to_response('dashboard/dashboard.html',{'form':form, 'mentors':mentors}, context_instance=RequestContext(request))
+	return render_to_response('dashboard/dashboard.html',{'form':form, 'mentors':mentors}, 
+			context_instance=RequestContext(request))
 
 
 @login_required
 def message(request):
+	submitted = False
 	if request.POST:
 		form = MessageForm(request.POST)
 		if form.is_valid():
 			user = User.objects.get(id = request.user.id)
 			linkedin_id = Profile.objects.get(user_id = user.id).linkedin_id
-			db = connect_db('MONGOLAB_URI', 'APP_NAME')
+			db = connect_mongodb()
 			client_code = db.authorized.find_one({'linkedin_id':linkedin_id})['client_code']
 			client_short_name = db.clients.find_one({'client_code':client_code})['short_name']
 			subject = form.cleaned_data['subject']
 			message = form.cleaned_data['message']
 			mentor_id = form.cleaned_data['mentor_id']
 			query = "db.%s.find_one({'linkedin_id':'%s'})" % (client_short_name, mentor_id)
-			mentor_email = eval(query)['email']
-			send_mail(subject, message, mentor_email, user.email)
+			mentor = eval(query)
+			send_mail(subject, message, mentor['email'], user.email)
+			submitted = True
 		else:
 			print form.errors
 	else:
 		mentor_id = request.GET['mi']
+		user = User.objects.get(id = request.user.id)
+		linkedin_id = Profile.objects.get(user_id = user.id).linkedin_id
+		db = connect_mongodb()
+		client_code = db.authorized.find_one({'linkedin_id':linkedin_id})['client_code']
+		client_short_name = db.clients.find_one({'client_code':client_code})['short_name']
+		query = "db.%s.find_one({'linkedin_id':'%s'})" % (client_short_name, mentor_id)
+		mentor = eval(query)
 		form = MessageForm()
-	return render_to_response('dashboard/message.html',{'form':form, 'mentor_id':mentor_id}, context_instance=RequestContext(request))
+	return render_to_response('dashboard/message.html',{'form':form, 'mentor':mentor, 
+			'mentor_id':mentor_id, 'submitted':submitted}, context_instance=RequestContext(request))
+
 
 def searchdb(request):
 	linkedin_id = Profile.objects.get(user_id=request.user.id).linkedin_id
-	db = connect_db('MONGOLAB_URI', 'APP_NAME')
+	db = connect_mongodb()
 	client_code = db.authorized.find_one({'linkedin_id':linkedin_id})['client_code']
 	client_short_name = db.clients.find_one({'client_code':client_code})['short_name']
 	query = request.GET['industry']
 	mentors = json.dumps(search_es(query, client_short_name))
-
 	return HttpResponse(json.dumps(mentors))
+
 
 @login_required
 def mentor_profile(request, linkedin_id):
-	db = connect_db('MONGOLAB_URI', 'APP_NAME')
+	db = connect_mongodb()
 	client_code = db.authorized.find_one({'linkedin_id':linkedin_id})['client_code']
 	client_short_name = db.clients.find_one({'client_code':client_code})['short_name']
 	query = "db.%s.find_one({'linkedin_id':'%s'})" % (client_short_name, linkedin_id)
 	mentor = eval(query)
-	return render_to_response('dashboard/mentor_profile.html', {'mentor':mentor, 'client_code':client_code}, context_instance=RequestContext(request))
+	return render_to_response('dashboard/mentor_profile.html', {'mentor':mentor, 
+			'client_code':client_code}, context_instance=RequestContext(request))
 
 
--- apps/profile/views.py
+++ apps/profile/views.py
@@ -24,7 +24,8 @@ import os
 @login_required
 def home(request):
 	profile = Profile.objects.get(user_id=request.user.id)
-	client_code = Authorized.objects.get(linkedin_id=profile.linkedin_id).client_code
+	db = connect_mongodb()
+	client_code = db.authorized.find_one({'linkedin_id':profile.linkedin_id})['client_code']
 	url = reverse('dashboard', args=(client_code,))
 	return redirect(url)
 
